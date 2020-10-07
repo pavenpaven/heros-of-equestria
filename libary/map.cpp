0581ef44d105228be0c5490cf310f89784317130
mattias burman <mmburman@hotmail.com>
2020-10-07 06:19:22 +0000

    Map Stuff, Map_node::generate, Map_node::get_contence()

--- libary/map.cpp
+++ libary/map.cpp
@@ -1,8 +1,21 @@
 #include <random>
+#include "map.h"
 
-Map::generate(int renderdist){
-	ammount=(rand()%3)+1
-	for (int i=0;i<ammunt;i++){
-		maps.push_back(Map());
-	}
+void Map_node::generate(int renderdist){
+	int ammount=(rand()%3)+1;
+  if(maps.size()==0){
+	  for (int i=0;i<ammount;i++){
+		  maps.push_back(Map_node());
+	  }
+  }
+  if (renderdist<1){
+    for(int i=0;i<maps.size();i++){
+      maps[i].generate(renderdist-1);
+    }
+  }
 }
+
+int Map_node::get_contence(){
+  return contence;
+}
+
--- libary/map.h
+++ libary/map.h
@@ -3,11 +3,15 @@
 
 #include <vector>
 
+
 class Map_node{
 public:
 	std::vector<Map_node> maps;
-	void generate(int renderdist);
-  int contence;
+  void generate(int renderdist);
+  int get_contence();
+  void print();
+private:
+  char contence;
 };
 
 #endif //MAP_H
