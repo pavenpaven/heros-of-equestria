a23fd2bf9803e7acc99f3df9fa809096561b5553
mattias.burman <mattias.burman@penguin>
2020-10-07 11:02:42 +0200

    input stuff promt(),

--- libary/input.cpp
+++ libary/input.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "input.h"
+
+static std::string error_list[]={
+	"","command not found. type 'help' for help","run_command not working check input.cpp"
+};
+
+// runs command, returns status
+static int run_command(){
+	return 2;
+}
+
+static void print_error(int error_inctence){
+	std::cout<<error_list[error_inctence];
+}
+
+int promt(){
+	bool is_in_loop=true;
+	std::string command_character;
+	int error;
+
+	while (is_in_loop){
+		std::cout<<">";
+		std::cin>> command_character;
+		error=run_command();
+		print_error(error);
+		
+	}
+
+	return 0;
+}
 
 std::string ask(std::string quest)
 {
--- libary/input.h
+++ libary/input.h
@@ -3,6 +3,10 @@
 
 #include <string>
 
+
+// promts the user, returns status
+int promt();
+
 std::string ask(std::string quest);
 
 std::string load(std::string filename);
